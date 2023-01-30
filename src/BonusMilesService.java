f8669af2f2b53587fa61285a1fd22797006e559d
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2023-01-30 20:57:19 +0300

    Used new class BonusMilesService

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+
+        int bonusMiles = 20;
+        int miles = cost / bonusMiles;
+        return miles;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketCost = 19_532;
-        int valueBonusMile = 20;
-        int amountOfBonusMiles = ticketCost / valueBonusMile;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Количество бонусных миль: " + miles);
 
-        System.out.println("Количество бонусных миль: " + amountOfBonusMiles);
     }
 
 }
